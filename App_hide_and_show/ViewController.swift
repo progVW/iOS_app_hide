0fcaf393cbfbb221748c7fb07f02fff155d7fe9d
SukharevS <suharev.s@icloud.com>
2024-08-19 20:53:33 +0300

    Added ViewActions and edit Main.storyboard

--- App_hide_and_show/ViewController.swift
+++ App_hide_and_show/ViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var showAllButton: UIButton!
+    
+    @IBOutlet weak var firstImage: UIImageView!
+    
+    @IBOutlet weak var secondImage: UIImageView!
+    
+    @IBOutlet weak var thirdImage: UIImageView!
+    
+    @IBOutlet weak var hideFirstButton: UIButton!
+    
+    @IBOutlet weak var hideSecondButton: UIButton!
+    
+    @IBOutlet weak var hideThirdButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func showAllAction(_ sender: Any) {
+        firstImage.isHidden = false
+        secondImage.isHidden = false
+        thirdImage.isHidden = false
+    }
+    
+    
+    
+    @IBAction func hideFirstAction(_ sender: Any) {
+        firstImage.isHidden = true
+    }
+    
+    
+    @IBAction func hideSecondAction(_ sender: Any) {
+        secondImage.isHidden = true
+    }
+    
+    
+    @IBAction func hideThirdAction(_ sender: Any) {
+        thirdImage.isHidden = true
+    }
 }
 
